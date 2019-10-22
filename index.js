a1a10d78a0c4cbd5d72dedacfec3abc594d9bf61
MarkusKerminen <m.j.kerminen@gmail.com>
2019-10-23 00:42:50 +0300

    removed morgan data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 let persons = [
   {
